f39df8cd1716502ccc1b01fe88ddd742101bc922
rosh-1228 <sho.watanabe1228@gmail.com>
2021-12-12 21:56:57 +0900

    config/localeにja.ymlを追加し、表示される英語を日本語に変換

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module BooksApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+    config.i18n.available_locales = [:en, :ja]
+    config.i18n.default_locale = :ja
   end
 end
