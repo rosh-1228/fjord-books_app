100c853df9279d55a2afe2dd95f8aa64aa88d870
rosh-1228 <sho.watanabe1228@gmail.com>
2021-12-30 22:27:53 +0900

    presence: trueのバリデーションを追記

--- app/models/follow_relationship.rb
+++ app/models/follow_relationship.rb
@@ -3,4 +3,6 @@
 class FollowRelationship < ApplicationRecord
   belongs_to :following, class_name: "User"
   belongs_to :followed, class_name: "User"
+  validates :following_id, presence: true
+  validates :followed_id, presence: true
 end
