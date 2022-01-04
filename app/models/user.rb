b2ac24734d58108c7328c7504324f8f7011eb043
rosh-1228 <sho.watanabe1228@gmail.com>
2022-01-04 22:33:00 +0900

    フォロー数、フォロワー数の表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,30 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  has_many :followeds,
+            class_name: :FollowRelationship,
+            foreign_key: 'followed_id',
+            dependent: :destroy,
+            inverse_of: :followed
+  has_many :followings,
+            class_name: :FollowRelationship,
+            foreign_key: 'following_id',
+            dependent: :destroy,
+            inverse_of: :following
+
+  has_many :followed_users, through: :follow_relationships, source: :followed
+  has_many :following_users, through: :follow_relationships, source: :following
+
+  def following?(other_user)
+    following_users.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    followed_users.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(user)
+    followed_users.find_by(followed_id: other_user.id).destroy
+  end
 end
