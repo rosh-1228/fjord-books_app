6797e80d04ead374ade177546ccf1c6dee0583bb
rosh-1228 <sho.watanabe1228@gmail.com>
2022-01-27 17:04:52 +0900

    日報にvalidationをかけていなかったので、修正

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,4 +4,9 @@ class Report < ApplicationRecord
   has_many :comments, as: :commentable, dependent: :destroy
 
   belongs_to :user
+
+  with_options presence: true do
+    validates :title
+    validates :report_content
+  end
 end
