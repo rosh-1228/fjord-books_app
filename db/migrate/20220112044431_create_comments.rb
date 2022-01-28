5e16332ba3bfb615825dbfda1ae92a09a513c237
rosh-1228 <sho.watanabe1228@gmail.com>
2022-01-28 11:17:22 +0900

    user_idをintegerからreferencesの指定に修正

--- db/migrate/20220112044431_create_comments.rb
+++ db/migrate/20220112044431_create_comments.rb
@@ -5,8 +5,8 @@ class CreateComments < ActiveRecord::Migration[6.1]
     create_table :comments do |t|
       t.references :commentable, polymophic: true, null: false
       t.string :commentable_type, null: false
-      t.text :comment_content
-      t.integer :user_id, foreign_key: true
+      t.text :comment_content, null: false
+      t.references :user_id, foreign_key: true, null: false
 
       t.timestamps
     end
