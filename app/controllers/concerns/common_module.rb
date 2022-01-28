ff795467022af5f738c947f0f1532487fe0fd10e
rosh-1228 <sho.watanabe1228@gmail.com>
2022-01-28 17:14:32 +0900

    routes.rbにてresources :books :reportsそれぞれの:commentsにmodule: :reportsをつけて、別々のコントローラでコメントの処理を実行するように修正

--- app/controllers/concerns/common_module.rb
+++ app/controllers/concerns/common_module.rb
@@ -6,14 +6,4 @@ module CommonModule
     load_model = params[:controller] == 'reports' ? Report.find(params[:id]): Book.find(params[:id])
     @comments = load_model.comments.joins(:user).preload(:user).select(:id, :created_at, :name, :email, :comment_content, :user_id)
   end
-
-  def collect_model_param
-    commentable_type = request.path.split('/')[1].singularize.capitalize
-    case commentable_type
-    when 'Book'
-      { id: params[:book_id], set_type: Book.find(params[:book_id]), path: book_path(params[:book_id]) }
-    when 'Report'
-      { id: params[:report_id], set_type: Report.find(params[:report_id]), path: report_path(params[:report_id]) }
-    end
-  end
 end
