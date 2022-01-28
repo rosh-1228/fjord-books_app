e53d0cfc7174547b9c39ca8137b839d90f2050a8
rosh-1228 <sho.watanabe1228@gmail.com>
2022-01-28 15:41:04 +0900

    コメント投稿者が表示しなくなっていたので、表示するようにset_commentsメソッドを修正

--- app/controllers/concerns/common_module.rb
+++ app/controllers/concerns/common_module.rb
@@ -4,7 +4,7 @@ module CommonModule
   extend ActiveSupport::Concern
   def set_comments(params)
     load_model = params[:controller] == 'reports' ? Report.find(params[:id]): Book.find(params[:id])
-    @comments = load_model.comments.joins(:user).preload(:user)
+    @comments = load_model.comments.joins(:user).preload(:user).select(:id, :created_at, :name, :email, :comment_content, :user_id)
   end
 
   def collect_model_param
