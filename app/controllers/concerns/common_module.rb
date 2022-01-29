15b00551638a39129c11663a0ecc9fccd00b05e9
rosh-1228 <sho.watanabe1228@gmail.com>
2022-01-29 09:28:13 +0900

    load_commentメソッドのselectを削除し、日報/本のshow.html.erbでコメント情報を表示できるように修正

--- app/controllers/concerns/common_module.rb
+++ app/controllers/concerns/common_module.rb
@@ -4,6 +4,6 @@ module CommonModule
   extend ActiveSupport::Concern
   def load_comments(params)
     load_model = params[:controller] == 'reports' ? Report.find(params[:id]) : Book.find(params[:id])
-    @comments = load_model.comments.joins(:user).preload(:user).select(:id, :created_at, :name, :email, :comment_content, :user_id)
+    @comments = load_model.comments.joins(:user).preload(:user)
   end
 end
